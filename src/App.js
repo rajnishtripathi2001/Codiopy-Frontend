196f495d66b10c1d0744b66f134dedb1df7fd9f3
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2023-07-02 00:26:21 +0530

    Footer section added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Header/>
+    <Footer/>
     </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Footer.css";
+import { FaFacebook, FaGoogle, FaGooglePlus, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-upper">
+        <div className="footer-left">
+          <div className="footer-left-top">
+            <h4>Your Logo</h4>
+            <p>Your Tagline here</p>
+          </div>
+
+          <div className="footer-left-bottom">
+            <h2>About Us</h2>
+            <p>We want to help you find the best products for your needs.</p>
+             
+            <h2>Contact Us</h2>
+            <p id="contact-source">Phone : 123-456-7890</p>
+            <p id="contact-source">Email : rakshd@hmla.com</p>
+          </div>
+
+        </div>
+        <div className="footer-center">
+          <h2>Informations</h2>
+          <p>About Us</p>
+          <p>Blog</p>
+          <p>Testimonoals</p>
+          <p>Events</p>
+        </div>
+        <div className="footer-right">
+          <h2>Helpful Links</h2>
+          <p>Services</p>
+          <p>Supports</p>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+        </div>
+        <div className="subscribe">
+          <h3>Subscribe more info</h3>
+          <input type="email" name="email" id="email" />
+          <button className="btn-subscribe">Subscribe</button>
+        </div>
+      </div>
+
+
+      <br />
+      <hr />
+      <br />
+
+      <div className="footer-lower">
+        <FaFacebook />
+        <FaGooglePlus/>
+        <FaTwitter/>
+        <FaInstagram/>
+
+      </div>
+    </div>
+  );
+}
